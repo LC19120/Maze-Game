4a6909c1d233a554542d9e65fa2bfca16dd26f21
lc19120 <lc19120@iCloud.com>
2025-12-24 18:12:21 +0800

    Include all system header

---
+++ include/core/Common.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <random>
+#include <expected>
+#include <filesystem>
+#include <mdspan>
+#include <print>
+#include <future>
+#include <mutex>
+
